9fa0a5bacf6ec8bd9265ca20dcded3d7a2170b37
Luan fonseca de farias <luanfonceca@gmail.com>
2017-06-21 08:21:37 -0300

    #3 - Add tests for the Destroy action.

--- todolist/tests/test_api.py
+++ todolist/tests/test_api.py
@@ -98,3 +98,16 @@ class ToDoListUpdatViewTest(TestCase):
 
         self.assertEqual(
             response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ToDoLisDestroyViewTest(TestCase):
+    def setUp(self):
+        self.todolist = mommy.make(ToDoList, title='Example list 1')
+
+    def test_destroy(self):
+        response = self.client.delete(
+            reverse('todolist:detail', kwargs={'pk': self.todolist.pk}))
+
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.data, None)
