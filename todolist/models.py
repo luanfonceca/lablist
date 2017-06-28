c81aef59f3ba200e3b116a5ce46bd4965322cc73
Luan fonseca de farias <luanfonceca@gmail.com>
2017-06-28 13:22:21 -0300

    #15 - Fix a bug when we have only one task created.

--- todolist/models.py
+++ todolist/models.py
@@ -16,10 +16,11 @@ class ToDoList(models.Model):
 
     @property
     def progress_ratio(self):
-        if not self.tasks.exists():
-            return None
-
         total = self.tasks.count()
         done_ones = self.tasks.filter(is_done=True).count()
+
+        if not total or not done_ones:
+            return None
+
         ratio = 100 * done_ones / total
         return '{:.1f}%'.format(ratio)
